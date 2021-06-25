57e899acc9ca582dfa8b6bf66a9bf719124057fb
Alexandra Cohen <alexandracohen100@gmail.com>
2021-06-25 14:22:34 +0100

    Updated

--- Index.js
+++ Index.js
@@ -1,9 +1,10 @@
-function displayTemperatre(response) {
-  let temperatureElement = document.querySelector("temperature");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("city");
   temperatureElement.innerHTML = math.round(response.data.main.temp);
 }
-cityElement.innerHTML = resonse.data.name;
+
 let apiKey = "c83f0b36646f8f213130092c5eb7cca0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric`;
-axios.get(apiUrl).then(displayTemperatre);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
