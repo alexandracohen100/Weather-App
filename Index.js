037bee0de17263dc0d746401d373f1921ff4f6d3
Alexandra Cohen <alexandracohen100@gmail.com>
2021-06-25 17:15:31 +0100

    Updates

--- Index.js
+++ Index.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "c83f0b36646f8f213130092c5eb7cca0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
